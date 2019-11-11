7a679b4e6d7a11459fd169b750140aee8ede012f
Dimitris Kouris <dskouris@gmail.com>
2019-11-11 10:08:56 +0000

    initial setup

---
+++ front-end/final-project/navigation/MainTabNavigator.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import AgendaScreen from '../screens/AgendaScreen';
+import ChatsScreen from '../screens/ChatsScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {}
+});
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen
+  },
+  config
+);
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  )
+};
+
+HomeStack.path = '';
+
+const AgendaStack = createStackNavigator(
+  {
+    Agenda: AgendaScreen
+  },
+  config
+);
+
+AgendaStack.navigationOptions = {
+  tabBarLabel: 'Agenda',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  )
+};
+
+AgendaStack.path = '';
+
+const ChatsStack = createStackNavigator(
+  {
+    Chats: ChatsScreen
+  },
+  config
+);
+
+ChatsStack.navigationOptions = {
+  tabBarLabel: 'Chats',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  )
+};
+
+ChatsStack.path = '';
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen
+  },
+  config
+);
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  )
+};
+
+SettingsStack.path = '';
+
+const tabNavigator = createBottomTabNavigator({
+  HomeStack,
+  AgendaStack,
+  ChatsStack,
+  SettingsStack
+});
+
+tabNavigator.path = '';
+
+export default tabNavigator;
---
+++ front-end/final-project/screens/ChatsScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+
+export default function ChatsScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      <Text>Chats here</Text>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff'
+  }
+});
