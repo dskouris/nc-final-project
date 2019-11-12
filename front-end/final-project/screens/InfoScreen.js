51bf572a7b33a476704805e472dceff6631d6ce8
Dimitris Kouris <dskouris@gmail.com>
2019-11-12 15:37:15 +0000

    completed navigation

--- front-end/final-project/screens/InfoScreen.js
+++ front-end/final-project/screens/InfoScreen.js
@@ -4,18 +4,17 @@ import { View, StyleSheet, Text, Button } from 'react-native';
 export default function InfoScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Info page</Text>
       <Button
         title='go back'
         onPress={() => navigation.navigate(navigation.getParam('back', 'Home'))}
       />
+      <Text>Info for individual location goes here</Text>
     </View>
   );
 }
 
 InfoScreen.navigationOptions = {
-  title: 'Info',
-  header: true
+  title: 'Info'
 };
 const styles = StyleSheet.create({
   container: {
