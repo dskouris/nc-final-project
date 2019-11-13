33a2edfbfbf42099e7dbc0b941fe61923c4e5803
Tom Kotlar <t.kotlar@yahoo.com>
2019-11-13 11:39:12 +0000

    adding content for InfoScreen

--- front-end/final-project/screens/InfoScreen.js
+++ front-end/final-project/screens/InfoScreen.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, Text, Button, Image } from "react-native";
 
-export default function InfoScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Button
-        title='go back'
-        onPress={() => navigation.navigate(navigation.getParam('back', 'Home'))}
-      />
-      <Text>Info for individual location goes here</Text>
-    </View>
-  );
+export default class InfoScreen extends Component {
+  state = {
+    location: {
+      name: "Manchester Cathedral",
+      img:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs3-media2.fl.yelpcdn.com%2Fbphoto%2FebnpeaM5JZbySlcxWCi1kQ%2Fo.jpg&f=1",
+      type: "Cathedral",
+      address: "Victoria Street, Manchester M3 1SX, City Centre",
+      coords: { lat: 53.4852373, long: -2.2465376 }
+    },
+    isGoing: false,
+    usersGoing: [
+      { name: "Tom", uuid: "jjfdjfd294ikvnknv" },
+      { name: "Dimitris", uuid: "jjfdjfdff294ikvnknv" },
+      { name: "Poulina", uuid: "jjfdjf55554i390" },
+      { name: "Sara", uuid: "j55djfd294i390" },
+      { name: "Krishan", uuid: "jj66jfd294i390" }
+    ],
+    userLocation: { lat: 53.4852373, long: -2.2465376 }
+  };
+
+  render() {
+    const { location, isGoing, usersGoing } = this.state;
+    return (
+      <View style={styles.container}>
+        <Button
+          title="go back"
+          onPress={() =>
+            this.props.navigation.navigate(navigation.getParam("back", "Home"))
+          }
+        />
+        <View>
+          <Text>{location.name}</Text>
+          <Image
+            source={{ uri: location.img }}
+            style={{ width: "80%", height: 200 }}
+          />
+          <Text>{usersGoing.length} Going</Text>
+          <Text>{isGoing ? "You Are Going" : "You Are Not Going"}</Text>
+         
+        </View>
+
+        <View> 
+        <Text>{location.type}</Text>
+        <Text>{location.address}</Text>
+        <Text>One km from Location</Text>
+        </View>
+        <View>
+          
+        </View>
+      </View>
+    );
+  }
 }
 
 InfoScreen.navigationOptions = {
-  title: 'Info'
+  title: "Info"
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   }
 });
