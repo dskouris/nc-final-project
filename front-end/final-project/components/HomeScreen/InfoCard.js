bff01151d87e498bcb6be5474590573ac34553ed
Dimitris Kouris <dskouris@gmail.com>
2019-11-19 10:55:07 +0000

    added distance from location to homescreen infocards

--- front-end/final-project/components/HomeScreen/InfoCard.js
+++ front-end/final-project/components/HomeScreen/InfoCard.js
@@ -1,25 +1,37 @@
-import React, { Component } from "react";
-import { Card, CardItem, Left, Thumbnail, Right } from "native-base";
-import { Text, View, ScrollView } from "react-native";
-import { Button } from "react-native-elements";
+import React, { Component } from 'react';
+import { Card, CardItem, Left, Thumbnail, Right } from 'native-base';
+import { Text, View, ScrollView } from 'react-native';
+import { Button } from 'react-native-elements';
+import * as utils from '../../utils/utils';
 
-const InfoCard = ({ location, navigation }) => {
+const InfoCard = ({ location, navigation, userCoords }) => {
+  let distanceFromLocation = utils.calculateDistance(
+    userCoords.latitude,
+    userCoords.longitude,
+    location.geometry.location.lat,
+    location.geometry.location.lng
+  );
   return (
     <View>
       <Card>
         <CardItem>
           <Left>
-            <Thumbnail source={require("../images/robot-dev.png")} />
+            <Thumbnail source={require('../images/robot-dev.png')} />
             <View>
               <Text>{location.name}</Text>
               <Text>0 going</Text>
+              <Text>{distanceFromLocation}km away</Text>
             </View>
           </Left>
 
           <Button
-            title="See more info"
+            title='See more info'
             onPress={() =>
-              navigation.navigate("Info", { back: "Home", location })
+              navigation.navigate('Info', {
+                back: 'Home',
+                location,
+                distanceFromLocation
+              })
             }
           />
         </CardItem>
