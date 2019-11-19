d6595d9c0fe5dd366c16a85ac93b8b4027dd20d8
poalina <paula828@op.pl>
2019-11-19 09:51:20 +0000

    updates in homescreen

--- front-end/final-project/components/SettingsScreen/ProfilePicture.js
+++ front-end/final-project/components/SettingsScreen/ProfilePicture.js
@@ -1,12 +1,11 @@
 import * as React from "react";
-import { Button, Image, View, Text} from "react-native";
+import { Button, Image, View, Text } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import Constants from "expo-constants";
 import * as Permissions from "expo-permissions";
 import { Input, Avatar, Badge } from "react-native-elements";
 
 export default class ProfilePicture extends React.Component {
-<<<<<<< HEAD
   // pickImage = async () => {
   //   let result = await ImagePicker.launchImageLibraryAsync({
   //     mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -22,21 +21,11 @@ export default class ProfilePicture extends React.Component {
   //   }
   // };
 
-
-
-
   // handleChange = () => {
   //   // console.log("handle change");
   //   this.props.pickImage();
   // };
 
-
-  
-=======
-  handleChange = () => {
-    this.props.pickImage();
-  };
->>>>>>> 72e3ab3cd17b0f8b0590bfea18161abcd4e3a12a
   render() {
     const { profilePicture } = this.props;
 
@@ -51,39 +40,27 @@ export default class ProfilePicture extends React.Component {
         <Button
           title={
             profilePicture
-<<<<<<< HEAD
               ? "Update a profile picture"
               : "1Pick a profile picture"
-=======
-              ? "Change your profile picture"
-              : "Pick your profile picture"
->>>>>>> 72e3ab3cd17b0f8b0590bfea18161abcd4e3a12a
           }
           onPress={() => {
             this.handleChange();
           }}
         />
         {profilePicture && (
-
           <Avatar
             size="xlarge"
             rounded
             source={{ uri: profilePicture }}
             // style={{ width: 200, height: 200 }}
             // activeOpacity={0.7}
-            containerStyle={{flex: 2, marginLeft: 20, }}
-
+            containerStyle={{ flex: 2, marginLeft: 20 }}
           />
-
-  
         )}
-              
-
-  />
+        />
       </View>
     );
   }
-<<<<<<< HEAD
 
   // componentDidMount() {
   //   this.getPermissionAsync();
@@ -91,14 +68,12 @@ export default class ProfilePicture extends React.Component {
   // }
 
   //FOR IOS ONLY
-//   getPermissionAsync = async () => {
-//     if (Constants.platform.ios) {
-//       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-//       if (status !== "granted") {
-//         alert("Sorry, we need camera roll permissions to make this work!");
-//       }
-//     }
-//   };
-=======
->>>>>>> 72e3ab3cd17b0f8b0590bfea18161abcd4e3a12a
+  //   getPermissionAsync = async () => {
+  //     if (Constants.platform.ios) {
+  //       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+  //       if (status !== "granted") {
+  //         alert("Sorry, we need camera roll permissions to make this work!");
+  //       }
+  //     }
+  //   };
 }
