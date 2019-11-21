20265346aa9f6ba09b80e9675eb045b6604a1e84
Tom Kotlar <t.kotlar@yahoo.com>
2019-11-21 16:19:39 +0000

    styling the infoscreen

--- front-end/final-project/components/HomeScreen/List.js
+++ front-end/final-project/components/HomeScreen/List.js
@@ -8,7 +8,6 @@ export default class List extends Component {
   render() {
     const { locations, navigation, userCoords } = this.props;
     return (
-<<<<<<< HEAD
       <ScrollView>
         <Content padder> 
         {locations.map(location => {
@@ -21,25 +20,6 @@ export default class List extends Component {
             />
           );
         })}
-=======
-      <ScrollView
-        style={{
-          marginHorizontal: 10,
-          marginTop: 10
-        }}
-      >
-        <Content padder>
-          {locations.map(location => {
-            return (
-              <InfoCard
-                navigation={navigation}
-                location={location}
-                userCoords={userCoords}
-                key={location.id}
-              />
-            );
-          })}
->>>>>>> 08559dff4e76939e034a31aea517142b836733c6
         </Content>
       </ScrollView>
     );
