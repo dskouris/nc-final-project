2e07f9032d953b86400a01b42684e5d9aa751526
poalina <paula828@op.pl>
2019-11-21 15:11:22 +0000

    updates list style

--- front-end/final-project/components/HomeScreen/List.js
+++ front-end/final-project/components/HomeScreen/List.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { Image, Text, ScrollView, View, Dimensions } from 'react-native';
+import React, { Component } from "react";
+import { Image, Text, ScrollView, View, Dimensions } from "react-native";
 
-import InfoCard from './InfoCard';
-import { Content } from 'native-base';
+import InfoCard from "./InfoCard";
+import { Content } from "native-base";
 
 export default class List extends Component {
   render() {
     const { locations, navigation, userCoords } = this.props;
     return (
       <ScrollView
-        style={{
-          marginHorizontal: 10,
-          marginTop: 10
-        }}
+      // style={{
+      //   marginHorizontal: 10,
+      //   marginTop: 10
+      // }}
       >
         <Content padder>
           {locations.map(location => {
@@ -29,61 +29,4 @@ export default class List extends Component {
       </ScrollView>
     );
   }
-}
-
-// const { height, width } = Dimensions.get("window");
-
-{
-  /* <View style={{ flex: 1, paddingTop: 20 }}> */
-}
-{
-  /* <Text
-            style={{ fontSize: 24, fontWeight: "700", paddingHorizontal: 20 }}
-          >
-            The most popular places
-          </Text>
-          <View style={{ height: 130, marginTop: 20 }}>
-            <ScrollView
-              horizontal={true}
-              showsHorizontalScrollIndicator={false}
-            >
-              <ListPopular location={locations[0]} />
-              <ListPopular location={locations[1]} />
-            </ScrollView>
-          </View> */
-}
-{
-  /* <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
-    <Text style={{ fontSize: 24, fontWeight: "700", paddingHorizontal: 20 }}>
-      List of places
-    </Text>
-    <Text style={{ fontWeight: "100", marginTop: 10 }}>
-      See interesting places in the city
-    </Text>
-    <ScrollView>
-      {locations.map(location => {
-        return (
-          <InfoCard
-            navigation={navigation}
-            location={location}
-            key={location.id}
-          />
-        );
-      })} */
-}
-{
-  /* <Image
-                source={cooking}
-                style={{
-                  flex: 1,
-                  borderRadius: 5,
-                  borderWidth: 1,
-                  borderColor: "#dddddd"
-                }}
-              /> */
-}
-{
-  /* </ScrollView>
-  </View>
-</View>; */
 }
