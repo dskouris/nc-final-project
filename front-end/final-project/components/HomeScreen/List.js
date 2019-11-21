98cb11b06135183dd8e78982c8f39659e96a8a51
Dimitris Kouris <dskouris@gmail.com>
2019-11-21 15:27:00 +0000

    adds number of users going to info cards

--- front-end/final-project/components/HomeScreen/List.js
+++ front-end/final-project/components/HomeScreen/List.js
@@ -8,26 +8,7 @@ export default class List extends Component {
   render() {
     const { locations, navigation, userCoords } = this.props;
     return (
-<<<<<<< HEAD
       <ScrollView>
-        <Content padder> 
-        {locations.map(location => {
-          return (
-            <InfoCard
-              navigation={navigation}
-              location={location}
-              userCoords={userCoords}
-              key={location.id}
-            />
-          );
-        })}
-=======
-      <ScrollView
-        style={{
-          marginHorizontal: 10,
-          marginTop: 10
-        }}
-      >
         <Content padder>
           {locations.map(location => {
             return (
@@ -39,7 +20,6 @@ export default class List extends Component {
               />
             );
           })}
->>>>>>> 08559dff4e76939e034a31aea517142b836733c6
         </Content>
       </ScrollView>
     );
