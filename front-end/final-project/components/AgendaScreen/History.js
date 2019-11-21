16fc3ad09bf826c5da9404111ae5f8e1c34ec4a6
Dimitris Kouris <dskouris@gmail.com>
2019-11-21 12:33:01 +0000

    sets up history tab

--- front-end/final-project/components/AgendaScreen/History.js
+++ front-end/final-project/components/AgendaScreen/History.js
@@ -1,33 +1,54 @@
-import React from "react";
-import { ScrollView, ImageBackground, StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import { ScrollView, ImageBackground, StyleSheet, Text } from 'react-native';
+import firebaseSDK from '../firebaseSDK';
+import HistoryCard from './HistoryCard';
+import Loading from '../HomeScreen/Loading';
+import * as api from '../api';
 
-import HistoryCard from "./HistoryCard";
-
-export default function History(props) {
-  return (
-    <ImageBackground
-      style={styles.container}
-      source={require("../images/landmarks.png")}
-    >
-      <ScrollView>
-        {props.history.map(place => {
-          return (
-            <HistoryCard
-              place={place}
-              key={place.id}
-              navigation={props.navigation}
-            />
-          );
-        })}
-      </ScrollView>
-    </ImageBackground>
-  );
+class History extends Component {
+  state = { user: {}, isLoading: true };
+  componentDidMount() {
+    const uid = firebaseSDK.uid;
+    return api
+      .getUserData(uid)
+      .then(user => this.setState({ user, isLoading: false }));
+  }
+  render() {
+    return this.state.isLoading ? (
+      <Loading />
+    ) : (
+      <ImageBackground
+        style={styles.container}
+        source={require('../images/landmarks.png')}
+      >
+        <ScrollView>
+          {this.state.user.Agenda.history.length ? (
+            this.state.user.Agenda.history.map(place => {
+              return (
+                <HistoryCard
+                  place={place}
+                  key={place.id}
+                  navigation={this.props.navigation}
+                />
+              );
+            })
+          ) : (
+            <Text style={{ textAlign: 'center', marginTop: 50 }}>
+              You need to visit some places first!{' '}
+            </Text>
+          )}
+        </ScrollView>
+      </ImageBackground>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: '#fff'
     // justifyContent: 'center'
   }
 });
+
+export default History;
