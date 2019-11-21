cb9c7dcd6ad4105bf6d1c15d6c8b5fa695bd7724
poalina <paula828@op.pl>
2019-11-21 10:33:41 +0000

    changes styling in history and plan cards

--- front-end/final-project/components/AgendaScreen/AgendaCard.js
+++ front-end/final-project/components/AgendaScreen/AgendaCard.js
@@ -1,45 +1,42 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container, Content, Card, CardItem, Button } from "native-base";
+import { Text, View, TouchableOpacity } from "react-native";
+import { Container, Content, Card, CardItem, Button, Right } from "native-base";
+import { Ionicons } from "@expo/vector-icons";
 
 const AgendaCard = ({ location, navigation }) => {
   return (
-    <Card>
-      <CardItem>
-        <Text>{location.name}</Text>
-
-        <Text>Going there on: {location.date}</Text>
-        <Button
-          info
-          iconRight
-          onPress={() =>
-            navigation.navigate("Info", {
-              back: "Agenda",
-              location: location.location,
-              isGoing: true
-            })
-          }
-        >
-          <Text> See info </Text>
-        </Button>
-      </CardItem>
-    </Card>
+    <View>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("Info", {
+            back: "Agenda",
+            location: location.location,
+            isGoing: true
+          })
+        }
+      >
+        <Card>
+          <CardItem
+            style={{ backgroundColor: "#ffe4c4", paddingHorizontal: 10 }}
+          >
+            <View>
+              <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                {location.name}
+              </Text>
+              <Text>Going there on: {location.date}</Text>
+            </View>
+            <Right>
+              <Ionicons
+                name="ios-arrow-forward"
+                size={20}
+                color="#DE4C5D"
+              ></Ionicons>
+            </Right>
+          </CardItem>
+        </Card>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 export default AgendaCard;
-
-// <View>
-//   <Text>{location.name}</Text>
-//   <Text>Going here on: {location.date}</Text>
-//   <Button
-//     title='see info'
-//     onPress={() =>
-//       navigation.navigate('Info', {
-//         back: 'Agenda',
-//         location: location.location,
-//         isGoing: true
-//       })
-//     }
-//   />
-// </View>
--- front-end/final-project/components/AgendaScreen/History.js
+++ front-end/final-project/components/AgendaScreen/History.js
@@ -1,20 +1,33 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, ImageBackground, StyleSheet } from "react-native";
 
 import HistoryCard from "./HistoryCard";
 
 export default function History(props) {
   return (
-    <ScrollView>
-      {props.history.map(place => {
-        return (
-          <HistoryCard
-            place={place}
-            key={place.id}
-            navigation={props.navigation}
-          />
-        );
-      })}
-    </ScrollView>
+    <ImageBackground
+      style={styles.container}
+      source={require("../images/landmarks.png")}
+    >
+      <ScrollView>
+        {props.history.map(place => {
+          return (
+            <HistoryCard
+              place={place}
+              key={place.id}
+              navigation={props.navigation}
+            />
+          );
+        })}
+      </ScrollView>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+    // justifyContent: 'center'
+  }
+});
